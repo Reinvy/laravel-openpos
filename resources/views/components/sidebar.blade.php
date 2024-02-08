65633d685a94256d203ca93d32a537bfd19899f0
Reinvy <satriaoner@gmail.com>
2024-02-08 21:14:45 +0700

    first commit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,19 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Open POS</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">OP</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="nav-item dropdown ">
+                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <!-- <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link" href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
+                    </li>
+                </ul> -->
+            </li>
+
+</div>
